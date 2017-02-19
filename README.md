d3e0f95343ce9c23979d8e11a48dca15b596d32d
TheDragonRing <thedragonring.bod@gmail.com>
2017-02-19 20:44:22 +1100

    Upload Files

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-alphahex
+Alphahex
+========
+---------
+This is the first website template I have ever made from scratch.
+
+Features:
+---------
+
+  **Responsive** — The site template is built to be both mobile friendly and desktop friendly, with different layouts that automatically adjust to your screen or windows size.
+
+  **Scripts** - It comes with a contact form that uses a responsive script to send emails using the mailto function, 'scroll to top' button, an automatically updating copyright (you will have to edit the main.js file to change it to your name!) and content that fades in at a comfortable speed when the page loads (JavaScript/jQuery).
+
+  **Blog** - The template has a self-managed blog template included, with posts and an RSS feed that are very easy to use and blend in with the look of the rest of the site.
+
+  _The use of these features is governed by the Creative Commons Attribution 4.0 International License. See the LICENSE.md file for more information._
+
+Downloading & Installing:
+-------------------------
+
+  **Downloading** - Go to https://thedragonring.me/projects, find Alphahex and click the download button.
+
+  **Installing** - Extract the downloaded files and move them to your site's root (or wherever you want it to go) and customize the file. Voila, done!
+
+  _Be careful when editing files! Sometimes just one line can break the whole template._
+
+Screenshot:
+-----------
+![Alphahex Screenshot](screenshot.png?raw=true)
---
+++ assets/js/main.js
@@ -0,0 +1,64 @@
+/*
+  Alphahex by TheDragonRing (thedragonring.me)
+  Copyright © 2017 TheDragonRing - Creative Commons Attribution 4.0 International License
+*/
+
+$(document).ready(function(){
+
+  var $fadeIn = $('[fadeIn]');
+  $fadeIn.removeAttr('fadeIn');
+  $fadeIn.hide();
+  $fadeIn.fadeIn(500);
+
+  $('[scrollToTop]').click(function(){
+    event.preventDefault();
+    $('html, body').animate({
+        scrollTop: 0
+    }, 500);
+  });
+
+  $('a').each(function(){
+    if($(this).attr('href') === ''){
+      $(this).click(function(){
+        event.preventDefault();
+      });
+    }
+  });
+
+  $.get('https://api.github.com/repos/thedragonring/alphahex/releases/latest', function(data){
+    $('#alphahex #download').attr('href', data.zipball_url);
+  });
+
+  $('[name=subject]').keypress(function(event){
+    if(event.which === 13){
+      $('[name=message]').focus();
+    }
+  });
+  $('#submit').hover(function(){
+    var $subject = $('[name=subject]').val();
+    var $body = $('[name=message]').val();
+    var $url = 'mailto:hello@thedragonring.me?subject=' + $subject + '&body=' + $body;
+    $(this).attr('href', $url);
+  });
+  $('#submit').click(function(){
+    if($(this).attr('href') === '#'){
+      event.preventDefault();
+    }
+  });
+  $('#reset').click(function(){
+    event.preventDefault();
+    $('[name=subject]').val('');
+    $('[name=message]').val('');
+  });
+
+  function getYear(){
+    var year = new Date().getFullYear();
+    if(year === 2017){
+      return year;
+    }else{
+      return '2017 - ' + year;
+    }
+  }
+  $('#copyright').html('<a href="http://thedragonring.me/alphahex">Content: &copy; ' + getYear() + ' Alphahex</a>' + ' <a href="https://thedragonring.me">Design: &copy; ' + getYear() + ' TheDragonRing</a>');
+
+});
